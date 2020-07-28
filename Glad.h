45cc03c358f3190eabba8ea8ce3900325f644cf5
Xemuth <hamonclem@hotmail.fr>
2020-07-28 14:58:39 +0200

    little change in order to load opengl using glad and not GX11

--- Glad.h
+++ Glad.h
@@ -1,9 +1,11 @@
 #ifndef _plugin_glad_Glad_h_
 #define _plugin_glad_Glad_h_
 #include "include/glad/glad.h"
-#if defined(GUI_X11) || defined(GUI_GTK)
-//#include "include/glad/glad_glx.h"
+#if defined(GUI_X11) || defined(GUI_GTK) || defined(PLATFORM_POSIX)
+#include "include/glad/glad_glx.h"
 #elif defined(PLATFORM_WIN32)
 #include "include/glad/glad_wgl.h"
 #endif
+
+
 #endif
