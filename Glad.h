99efbf13fca398b4839ef59b733f8100e898616a
Xemuth <hamonclem@hotmail.fr>
2020-07-28 16:01:07 +0200

    should now work properly on linux/windows

--- Glad.h
+++ Glad.h
@@ -1,9 +1,9 @@
 #ifndef _plugin_glad_Glad_h_
 #define _plugin_glad_Glad_h_
 #include "include/glad/glad.h"
-#if defined(GUI_X11) || defined(GUI_GTK) || defined(PLATFORM_POSIX)
+#if defined(__linux__)
 #include "include/glad/glad_glx.h"
-#elif defined(PLATFORM_WIN32)
+#elif defined(_WIN32)
 #include "include/glad/glad_wgl.h"
 #endif
 
